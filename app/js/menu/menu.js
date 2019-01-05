782dbc5ce85fa67c1d96ab1147dabb175603707b
Maynard Lee <lxd_ddcome@163.com>
2019-01-05 09:24:29 +0800

    更新代码

---
+++ app/js/menu/menu.js
@@ -0,0 +1,17 @@
+function TuYaMenu() {
+    var obj = new Object();
+    obj.data = [];
+    obj.init = function() {
+
+    };
+    obj.beforeRender = function() {
+
+    };
+    obj.render = function() {
+
+    };
+    obj.afterRender = function() {
+
+    };
+    return obj;
+}
